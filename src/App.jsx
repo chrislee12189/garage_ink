cce28d2857a0a1842823588b7093b71c778665a0
chrislee12189 <12189@coderacademy.edu.au>
2023-02-13 16:54:29 +1000

    added photos to the assets folder for use across homepage and gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './styles';
 import { Navbar } from './components';
+import { teneileTattoos } from './assets';
 
 const App = () => (
 <div className="bg-primary overflow-hidden w-full">
   <div className={`${styles.paddingX} ${styles.paddingY}`}>
     <div className="max-w-auto">
       <Navbar />
+      <img src={teneileTattoos[6]}/>
     </div>
   </div>
 </div>
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,217 @@
 //all images for website stored here and exported under easily useable name
+import Amity_Profilepic from './Amity_Profilepic.jpg';
+import amity_tattoo1 from './amity_tattoo1.jpg';
+import Briar_Profilepic from './Briar_Profilepic.jpg';
+import briar_tattoo1 from './briar_tattoo1.jpg';
+import briar_tattoo2 from './briar_tattoo2.jpg';
+import briar_tattoo3 from './briar_tattoo3.jpg';
+import briar_tattoo4 from './briar_tattoo4.jpg';
+import briar_tattoo6 from './briar_tattoo6.jpg';
+import briar_tattoo7 from  './briar_tattoo7.jpg';
+import briar_tattoo8 from './briar_tattoo8.jpg';
+import briar_tattoo9 from './briar_tattoo9.jpg';
+import cim_tattoo1 from './cim_tattoo1.jpg';
+import cim_tattoo2 from './cim_tattoo2.jpg';
+import cim_tattoo3 from './cim_tattoo3.jpg';
+import crit_tattoo1 from './crit_tattoo1.jpg';
+import crit_tattoo2 from './crit_tattoo2.jpg';
+import crit_tattoo3 from './crit_tattoo3.jpg';
+import crit_tattoo4 from './crit_tattoo4.jpg';
+import crit_tattoo5 from './crit_tattoo5.jpg';
+import crit_tattoo6 from './crit_tattoo6.jpg';
+import crit_tattoo7 from './crit_tattoo7.jpg';
+import crit_tattoo8 from './crit_tattoo8.jpg';
+import Emily_Profilepic from './Emily_Profilepic.jpg';
+import em_tattoo1 from './em_tattoo1.jpg';
+import em_tattoo2 from './em_tattoo2.jpg';
+import em_tattoo3 from './em_tattoo3.jpg';
+import em_tattoo4 from './em_tattoo4.jpg';
+import em_tattoo5 from './em_tattoo5.jpg';
+import em_tattoo6 from './em_tattoo6.jpg';
+import em_tattoo7 from './em_tattoo7.jpg';
+import em_tattoo8 from './em_tattoo8.jpg';
+import em_tattoo9 from './em_tattoo9.jpg';
+import em_tattoo10 from './em_tattoo10.jpg';
+import giorgia_tattoo1 from './giorgia_tattoo1.jpg';
+import giorgia_tattoo2 from './giorgia_tattoo2.jpg';
+import giorgia_tattoo3 from './giorgia_tattoo3.jpg';
+import giorgia_tattoo4 from './giorgia_tattoo4.jpg';
+import giorgia_tattoo5 from './giorgia_tattoo5.jpg';
+import giorgia_tattoo6 from './giorgia_tattoo6.jpg';
+import Juannita_Profilepic from './Juannita_Profilepic.jpg';
+import juannita_tattoo1 from './juannita_tattoo1.jpg';
+import juannita_tattoo2 from './juannita_tattoo2.jpg';
+import juannita_tattoo3 from './juannita_tattoo3.jpg';
+import Karlee_Profilepic from './Karlee_Profilepic.jpg';
+import karlee_tattoo1 from './karlee_tattoo1.jpg';
+import karlee_tattoo2 from './karlee_tattoo2.jpg';
+import karlee_tattoo3 from './karlee_tattoo3.jpg';
+import karlee_tattoo4 from './karlee_tattoo4.jpg';
+import karlee_tattoo5 from './karlee_tattoo5.jpg';
+import karlee_tattoo6 from './karlee_tattoo6.jpg';
+import karlee_tattoo7 from './karlee_tattoo7.jpg';
+import Kimberly_Profilepic from './Kimberly_Profilepic.jpg';
+import kim_tattoo1 from './kim_tattoo1.jpg';
+import kim_tattoo2 from './kim_tattoo2.jpg';
+import kim_tattoo3 from './kim_tattoo3.jpg';
+import kim_tattoo4 from './kim_tattoo4.jpg';
+import Michelle_Profilepic from './Michelle_Profilepic.jpg';
+import michelle_tattoo1 from './michelle_tattoo1.jpg';
+import michelle_tattoo2 from './michelle_tattoo2.jpg';
+import michelle_tattoo3 from './michelle_tattoo3.jpg';
+import michelle_tattoo4 from './michelle_tattoo4.jpg';
+import michelle_tattoo5 from './michelle_tattoo5.jpg';
+import michelle_tattoo6 from './michelle_tattoo6.jpg';
+import michelle_tattoo7 from './michelle_tattoo7.jpg';
+import Paytan_Profilepic from './Paytan_Profilepic.jpg';
+import paytan_tatoo1 from './paytan_tattoo1.jpg';
+import paytan_tatoo2 from './paytan_tattoo2.jpg';
+import paytan_tatoo3 from './paytan_tattoo3.jpg';
+import paytan_tatoo4 from './paytan_tattoo4.jpg';
+import paytan_tatoo5 from './paytan_tattoo5.jpg';
+import Tayler_Profilepic from './Tayler_Profilepic.jpg';
+import tay_tattoo1 from './tay_tattoo1.jpg';
+import tay_tattoo2 from './tay_tattoo2.jpg';
+import tay_tattoo3 from './tay_tattoo3.jpg';
+import tay_tattoo4 from './tay_tattoo4.jpg';
+import tay_tattoo5 from './tay_tattoo5.jpg';
+import tay_tattoo6 from './tay_tattoo6.jpg';
+import tay_tattoo7 from './tay_tattoo7.jpg';
+import Teneile_Profilepic from './Teneile_Profilepic.jpg';
+import teneile_tattoo1 from './teneile_tattoo1.jpg';
+import teneile_tattoo2 from './teneile_tattoo2.jpg';
+import teneile_tattoo3 from './teneile_tattoo3.jpg';
+import teneile_tattoo4 from './teneile_tattoo4.jpg';
+import teneile_tattoo5 from './teneile_tattoo5.jpg';
+import teneile_tattoo6 from './teneile_tattoo6.jpg';
+import teneile_tattoo7 from './teneile_tattoo7.jpg';
+
+
+
+export const profilePics = [
+    Amity_Profilepic,
+    Briar_Profilepic,
+    Emily_Profilepic,
+    Juannita_Profilepic,
+    Kimberly_Profilepic,
+    Karlee_Profilepic,
+    Michelle_Profilepic,
+    Paytan_Profilepic,
+    Tayler_Profilepic,
+    Teneile_Profilepic,
+]
+
+export const amityTattoos = [
+    amity_tattoo1
+]
+
+export const briarTattoos =[
+    briar_tattoo1,
+    briar_tattoo2,
+    briar_tattoo3,
+    briar_tattoo4,
+    briar_tattoo6,
+    briar_tattoo7,
+    briar_tattoo8,
+    briar_tattoo9
+]
+
+export const cimTattoos = [
+    cim_tattoo1,
+    cim_tattoo2,
+    cim_tattoo3 
+]
+
+export const critTattoos = [
+    crit_tattoo1,
+    crit_tattoo2,
+    crit_tattoo3,
+    crit_tattoo4,
+    crit_tattoo5,
+    crit_tattoo6,
+    crit_tattoo7,
+    crit_tattoo8
+]
+
+export const emTattoos = [
+    em_tattoo1, 
+    em_tattoo2, 
+    em_tattoo3, 
+    em_tattoo4, 
+    em_tattoo5, 
+    em_tattoo6,
+    em_tattoo7, 
+    em_tattoo8, 
+    em_tattoo9, 
+    em_tattoo10
+]
+
+export const  giorgiaTattoos = [
+    giorgia_tattoo1,
+    giorgia_tattoo2,
+    giorgia_tattoo3,
+    giorgia_tattoo4,
+    giorgia_tattoo5,
+    giorgia_tattoo6,
+]
+
+export const juannitaTattoos = [
+    juannita_tattoo1,
+    juannita_tattoo2,
+    juannita_tattoo3
+]
+
+export const karleeTattoos = [
+    karlee_tattoo1,
+    karlee_tattoo2,
+    karlee_tattoo3,
+    karlee_tattoo4,
+    karlee_tattoo5,
+    karlee_tattoo6,
+    karlee_tattoo7
+]
+
+export const kimberlyTattoos = [
+    kim_tattoo1,
+    kim_tattoo2,
+    kim_tattoo3,
+    kim_tattoo4
+]
+
+export const michelleTattoos = [
+    michelle_tattoo1,
+    michelle_tattoo2,
+    michelle_tattoo3,
+    michelle_tattoo4,
+    michelle_tattoo5,
+    michelle_tattoo6,
+    michelle_tattoo7
+]
+
+export const paytanTattoos = [
+    paytan_tatoo1,
+    paytan_tatoo2,
+    paytan_tatoo3,
+    paytan_tatoo4,
+    paytan_tatoo5
+]
+
+export const taylerTattoos = [
+    tay_tattoo1,
+    tay_tattoo2,
+    tay_tattoo3,
+    tay_tattoo4,
+    tay_tattoo5,
+    tay_tattoo6,
+    tay_tattoo7
+]
+
+export const teneileTattoos = [
+    teneile_tattoo1,
+    teneile_tattoo2,
+    teneile_tattoo3,
+    teneile_tattoo4,
+    teneile_tattoo5,
+    teneile_tattoo6,
+    teneile_tattoo7,
+]
